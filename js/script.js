2ead2bb43add3a515f83d31e7c5424f1a082a962
unknown <akamakasin@gmail.com>
2023-02-07 22:31:09 +0700

    Добавил кнопки навигации и точки пагинации

---
+++ js/script.js
@@ -0,0 +1,12 @@
+new Swiper(".slider-main", {
+    sliderPerView: 6,  
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    },
+
+    // pagination: {
+    //     el: '.swiper-pagination',
+    //   },
+
+});
